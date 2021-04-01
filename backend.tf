4da423a73b47ea08ee2ae49728a66ee56aea57e1
fortnox13 <nekitdem@gmail.com>
2021-04-01 14:55:53 +0300

    laba

---
+++ backend.tf
@@ -0,0 +1,12 @@
+t!!erraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "terraformstatebucket6655870011"
+  }
+}
